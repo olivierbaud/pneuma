eaf6497bf28eeb95571c76cb9523403a869f7df1
olivierbaud <baud.olivier@gmail.com>
2019-09-25 15:46:06 +0200

    working on navbar

--- resources/java/andreabaud.js
+++ resources/java/andreabaud.js
@@ -1,19 +1,30 @@
  window.onscroll = function() {stickyMenu()};
-                
+// window.onscroll = function(){offsetDiv.forEach(element=>{element.style.height='0px'})}               
 var navbar = document.getElementById("navbar_an");
 var sticky = navbar.offsetTop;
-var citation = document.getElementsByClassName("citation")
+var offsetDiv = document.querySelectorAll('.offset')
 var hongrie = document.getElementById("an_hongrie")
-
+var links=navbar.querySelectorAll('a')
+//var divheight= offsetDiv.style.height
 function stickyMenu() {
   if (window.pageYOffset >= sticky-120) {
-  navbar.classList.add("sticky")
-  citation[1].style.top=240
+  navbar.classList.add("sticky")  
+  offsetDiv.forEach(element=>{element.style.height='120px'})
   } else {
   navbar.classList.remove("sticky");
+  offsetDiv.forEach(element=>{element.style.height='0px'})
   }
 }
+links.forEach(element=>{
+  
+})
 
-  navbar.onmouseup = function(){scrollBy(0,150)}
+// links.forEach(element=>{
+//   element.onclick=function(){    
+//     console.log(element)
+//     scrollBy(0,-220); 
+//   }
+// })
+  //navbar.onmouseup = function(){scrollBy(0,150)}
 
-  console.log(citation)
+  //console.log(links)
