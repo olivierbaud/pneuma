838e940d89838b8b221df3a1e5868a982f2b0414
olivierbaud <baud.olivier@gmail.com>
2019-09-25 18:22:18 +0200

    working on anchors

--- resources/java/andreabaud.js
+++ resources/java/andreabaud.js
@@ -2,22 +2,22 @@
 // window.onscroll = function(){offsetDiv.forEach(element=>{element.style.height='0px'})}               
 var navbar = document.getElementById("navbar_an");
 var sticky = navbar.offsetTop;
-var offsetDiv = document.querySelectorAll('.offset')
-var hongrie = document.getElementById("an_hongrie")
-var links=navbar.querySelectorAll('a')
+//var offsetDiv = document.querySelectorAll('.offset')
+//var hongrie = document.getElementById("an_hongrie")
+//var links=navbar.querySelectorAll('a')
 //var divheight= offsetDiv.style.height
 function stickyMenu() {
   if (window.pageYOffset >= sticky-120) {
   navbar.classList.add("sticky")  
-  offsetDiv.forEach(element=>{element.style.height='120px'})
+  //offsetDiv.forEach(element=>{element.style.height='120px'})
   } else {
   navbar.classList.remove("sticky");
-  offsetDiv.forEach(element=>{element.style.height='0px'})
+  //offsetDiv.forEach(element=>{element.style.height='0px'})
   }
 }
-links.forEach(element=>{
+//links.forEach(element=>{
   
-})
+//})
 
 // links.forEach(element=>{
 //   element.onclick=function(){    
